988b0421c5cbda77d03c2998bf4431a52de5b082
IkkiStar <122392793+IkkiStar@users.noreply.github.com>
2023-02-02 11:18:14 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -15,72 +15,27 @@ function runTimer(countdownElem){
 
     // This value will not change and creates the 5 second delay 
     var timeoutIncrement = 5000;
-
+    
+    for(var counter=0; counter <11; counter++){
       // Decreases the countdown by 5 on the page
       setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-               currTime = currTime - 5;
-       }, timeout);
-       timeout = timeout + timeoutIncrement;
-
-       setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-               currTime = currTime - 5;
-       }, timeout);
-       timeout = timeout + timeoutIncrement;
-
-       setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-               currTime = currTime - 5;
-       }, timeout);
-       timeout = timeout + timeoutIncrement;
-
-       setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-               currTime = currTime - 5;
-       }, timeout);
-       timeout = timeout + timeoutIncrement;
-
-       setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-               currTime = currTime - 5;
-       }, timeout);
-       timeout = timeout + timeoutIncrement;
-
-       setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-               currTime = currTime - 5;
-       }, timeout);
-       timeout = timeout + timeoutIncrement;
-
-       setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-               currTime = currTime - 5;
-       }, timeout);
-       timeout = timeout + timeoutIncrement;
-
-       setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-               currTime = currTime - 5;
-       }, timeout);
-       timeout = timeout + timeoutIncrement;
-
-       setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-               currTime = currTime - 5;
-       }, timeout);
-       timeout = timeout + timeoutIncrement;
-
-       setTimeout(function(){
-        countdownElem.innerHTML = currTime;
-               currTime = currTime - 5;
-       }, timeout);
-       timeout = timeout + timeoutIncrement;
-
-       // This is the last time we need to call setTimeout, currTime is now 0
-       setTimeout(function(){
-           alert("Blastoff!");
+       // if current time reaches 0 the blastoff alert will pop up and the countdown will say launch successful
+       if(currTime == 0){
+              alert("Blastoff!");
         countdownElem.innerHTML = "Launch successful!";
+       }
+       // when the current time is less than 25 the warning will pop up displaying the warning text and currTime
+       else if (currTime <25){
+              countdownElem.innerHTML = "Warning Less than 1/2 way to launch, time left = " + currTime;
+       }
+       // display the current time in the page
+       else{
+              countdownElem.innerHTML = currTime;
+       }
+        
+        //currTime decreases by 5
+               currTime = currTime - 5;
        }, timeout);
-
+       timeout = timeout + timeoutIncrement;
 }
+       }
